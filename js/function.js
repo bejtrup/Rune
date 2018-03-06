a125dc0983c05eea56f09a87490a8955ce46cc13
daniel <bejtrup@gmail.com>
2018-03-06 19:05:42 +0100

    find

--- js/function.js
+++ js/function.js
@@ -6,3 +6,27 @@ function fire() {
   $("input#wordOne").val(words[0]);
   $("input#wordTwo").val(words[1]);
 }
+function find(){
+  var wordOne = $.trim( $("input#wordOne").val() );
+  var WordTwo = $.trim( $("input#wordTwo").val() );
+  var x = getXYfromWord(wordOne);
+  var y = getXYfromWord(WordTwo);
+  var latlng = GetCoorFromXY(x,y);
+  drawSquare(latlng);
+  map.flyTo(latlng);
+}
+
+// autocomplete
+
+// share
+
+//  menu
+
+// rune Inder
+
+// css
+// zoombtn
+
+// sw
+
+// apk
--- js/map.js
+++ js/map.js
@@ -9,20 +9,8 @@ var OneMeter = (250/347)/10;
 
 function initmap() {
 
-  // map = L.map('map',{
-  //         minZoom: 0,
-  //         maxZoom: 5,
-  //         center: [0, 0],
-  //         zoom: 5,
-  //         crs: L.CRS.Simple
-  // }).setView([0, 0]);
-  //
-  // L.tileLayer.wms('kort/kort18_2/TileGroup0/{z}{x}{y}.jpg', {
-  //   attribution: 'Map data &copy; ???',
-  // }).addTo(map);
-
-var maxZoom = 8;
-var minZoom = 1;
+  var maxZoom = 8;
+  var minZoom = 1;
 
   map = L.map('map', {
     tap: true,
@@ -38,8 +26,8 @@ var minZoom = 1;
   }).addTo(map);
   // dimensions of the image
   var w = 4000,
-      h = 2834, //2831
-      url = 'kort18_2.png';
+  h = 2834, //2831
+  url = 'kort18_2.png';
   // calculate the edges of the image, in coordinate space
 
   var southWest = map.unproject([0, h], map.getMaxZoom()-(maxZoom/2));
@@ -57,28 +45,8 @@ var minZoom = 1;
   var b = new L.LatLngBounds(sw, ne);
   map.setMaxBounds(b);
   map.panTo([-75.268,98]);
-
-// ZOOMIFY
-
-  //  var w = 30000, //orginal: 3300
-  //      h = 21212, // orginal: 2450
-  //      url = 'kort/kort18/';
-  //  // calculate the edges of the image, in coordinate space
-  //  var offest = 5000;
-  //  var southWest = map.unproject([(-1*offest), (h+offest)], map.getMaxZoom());
-  //  var northEast = map.unproject([(w+offest), (-1*offest)], map.getMaxZoom());
-  //  var bounds = new L.LatLngBounds(southWest, northEast);
-  //
-  // L.tileLayer.zoomify(url, {
-  //     width: w,
-  //     height: h,
-  //     tolerance: 1,
-  // }).addTo(map);
-  //
-  // map.setMaxBounds(bounds);
-
-  // 40 meter = 173,9 - 176,6 = 2,7 -- 10m = 0,675 // zommify
 }
+
 function drawSquare(latLng){
   if(square) square.remove();
   circle = new L.Circle(latLng, OneMeter*5).addTo(map);
@@ -100,4 +68,10 @@ function getScuareXYFromCoor(lat, lng){
   var squareX = Math.floor( x/10 );
   var squareY = Math.floor( y/10 );
   return [squareX,squareY];
+}
+
+function GetCoorFromXY(x,y){
+  var ln = ((x * 10)+5)*OneMeter;
+  var la = -((y * 10)-5)*OneMeter;
+  return new L.LatLng(la, ln);
 }
