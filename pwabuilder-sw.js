22aa0463b9bd527088525814cc6bb8804df5ce37
daniel <bejtrup@gmail.com>
2018-06-01 14:10:31 +0200

    sw

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,4 +1,4 @@
-var version = 12;
+var version = 16;
 var CACHE = 'RUNE_cache-only';
 
 self.addEventListener('install', function(evt) {
@@ -17,68 +17,64 @@ self.addEventListener('install', function(evt) {
 self.addEventListener('fetch', function(evt) {
    console.log('The service worker is serving the asset. V.'+version);
    //evt.respondWith(fromCache(evt.request));
-
-
-  // var updateCache = function(request){
-  //   return caches.open(CACHE).then(function (cache) {
-  //     return fetch(request).then(function (response) {
-  //       console.log('add page to offline'+response.url)
-  //       return cache.put(request, response);
-  //     });
-  //   });
-  // };
-  //
-  //evt.waitUntil(updateCache(evt.request));
-
-  evt.respondWith(
-    fetch(evt.request).catch(function(error) {
-      console.log( 'Network request Failed. Serving content from cache: ' + error );
-
-      //Check to see if you have it in the cache
-      //Return response
-      //If not in the cache, then return error page
-      return caches.open(CACHE).then(function (cache) {
-        return cache.match(evt.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
-        });
+  var updateCache = function(request){
+    return caches.open(CACHE).then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log('add page to offline'+response.url)
+        return cache.put(request, response);
       });
-    }))
+    });
+  };
+
+  evt.waitUntil(updateCache(evt.request));
+
+evt.respondWith(fromCache(evt.request).catch(function(error) {console.log( 'nt from cache: ' + error ); } ) );
+
+  // evt.respondWith(
+  //   fetch( evt.request ).catch(function(error) {
+  //     console.log( 'Network request Failed. Serving content from cache: ' + error );
+  //     return caches.open(CACHE).then(function (cache) {
+  //       return cache.match(evt.request).then(function (matching) {
+  //         var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+  //         return report
+  //       });
+  //     });
+  //   }))
 
 });
 
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './index.html',
-      './kort18_2.png',
-      './manifest.json',
-       './css/HVD_Poster_Clean.ttf',
-      './css/index.css',
-      './img/border.png',
-      './img/MobilePay_Logo.png',
-      './img/rune_arm.png',
-      './img/rune_close.png	',
-      './img/rune_get.png',
-      './img/rune_info.png',
-      './img/rune_menu.png',
-      './img/rune_post.png',
-      './img/rune_screen.png',
-      './img/rune_share.png',
-      './img/square.png',
-      './js/function.js',
-      './js/map.js',
-      './js/share.js',
-      './js/words.js',
-      './js/jquery.min.js',
-      './js/leaflet1.3.1.js'
+      // './index.html',
+      //'./kort18_2.png',
+      // './manifest.json',
+      //  './css/HVD_Poster_Clean.ttf',
+      // './css/index.css',
+      // './img/border.png',
+      // './img/MobilePay_Logo.png',
+      // './img/rune_arm.png',
+      // './img/rune_close.png	',
+      // './img/rune_get.png',
+      // './img/rune_info.png',
+      // './img/rune_menu.png',
+      // './img/rune_post.png',
+      // './img/rune_screen.png',
+      // './img/rune_share.png',
+      // './img/square.png',
+      // './js/function.js',
+      // './js/map.js',
+      // './js/share.js',
+      // './js/words.js',
+      // './js/jquery.min.js',
+      // './js/leaflet1.3.1.js'
     ]);
   });
 }
 
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
-    console.log(CACHE, "cas",cache);
+    console.log(CACHE, "casHAHAH",cache);
     return cache.match(request).then(function (matching) {
       return matching || Promise.reject('no-match-du');
     });
