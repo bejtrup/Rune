df99c25193ea232c8162d07b4674e1a50f40d69e
daniel <bejtrup@gmail.com>
2018-06-02 11:20:55 +0200

    sw null

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,34 +1,36 @@
-var version = 16;
+var version = 19;
 var CACHE = 'RUNE_cache-only';
 
 self.addEventListener('install', function(evt) {
    console.log('The service worker is being installed. v.'+version);
-  var indexPage = new Request('index.html');
-  evt.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open(CACHE).then(function(cache) {
-        console.log('Cached index page during Install'+ response.url);
-        return cache.put(indexPage, response);
-      });
-  }));
+  // var indexPage = new Request('index.html');
+  // evt.waitUntil(
+  //   fetch(indexPage).then(function(response) {
+  //     return caches.open(CACHE).then(function(cache) {
+  //       console.log('Cached index page during Install'+ response.url);
+  //       return cache.put(indexPage, response);
+  //     });
+  // }));
+  //
   //evt.waitUntil(precache());
 });
 
 self.addEventListener('fetch', function(evt) {
    console.log('The service worker is serving the asset. V.'+version);
    //evt.respondWith(fromCache(evt.request));
-  var updateCache = function(request){
-    return caches.open(CACHE).then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log('add page to offline'+response.url)
-        return cache.put(request, response);
-      });
-    });
-  };
 
-  evt.waitUntil(updateCache(evt.request));
+  // var updateCache = function(request){
+  //   return caches.open(CACHE).then(function (cache) {
+  //     return fetch(request).then(function (response) {
+  //       console.log('add page to offline'+response.url)
+  //       return cache.put(request, response);
+  //     });
+  //   });
+  // };
+  //
+  // evt.waitUntil(updateCache(evt.request));
 
-evt.respondWith(fromCache(evt.request).catch(function(error) {console.log( 'nt from cache: ' + error ); } ) );
+  //evt.respondWith( fromCache(evt.request).catch(function(error) {console.log( 'OFFLINE!!! ' + error ); } ) );
 
   // evt.respondWith(
   //   fetch( evt.request ).catch(function(error) {
